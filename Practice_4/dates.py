1bb2b1a0d83dc5bc5798a4398cec22175135fa85
Akakhmet <ak_akhmet@kbtu.kz>
2026-03-06 13:36:47 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

--- Practice_4/dates.py
+++ Practice_4/dates.py
@@ -1,11 +1,39 @@
-from datetime import datetime
+import datetime
 
-now = datetime.now()
-print("Current time:", now)
+x = datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.strftime("%A"))
+print(x.strftime("%a"))
+print(x.strftime("%W"))
 
-formatted = now.strftime("%Y-%m-%d %H:%M")
-print("Formatted:", formatted)
+import datetime
+x = datetime.datetime(2020, 5 , 17)
+print(x)
 
-birthday = datetime(2006, 5, 1)
-difference = now - birthday
-print("Days lived:", difference.days)
+#1
+from datetime import datetime, timedelta
+
+today = datetime.now()
+new_date = today - timedelta(days = 5)
+print(new_date)
+
+#2
+from datetime import datetime, timedelta
+yesterday = today - timedelta(days = 1)
+today = datetime.now()
+tomorrow = today + timedelta(days =1)
+print(yesterday)
+print(today)
+print(tomorrow)
+
+#3
+import datetime
+now = datetime.datetime.now()
+print(now.replace(microsecond=0))
+
+#4
+import datetime
+d1 = datetime.datetime.strptime(input(),"%Y-%m-%d %H:%M:%S")
+d2 = datetime.datetime.strptime(input(),"%Y-%m-%d %H:%M:%S")
+print(int(abs(d2-d1).total_seconds()))
--- Practice_4/math.py
+++ Practice_4/math.py
@@ -1,12 +1,27 @@
+#1
 import math
-import random
 
-print("Min:", min(3, 7, 1))
-print("Sqrt:", math.sqrt(16))
-print("Pi:", math.pi)
+deg = float(input())
+rad = deg * math.pi/180
+print(rad)
 
-print("Random number:", random.randint(1, 10))
+#2
+h = float(input())
+a =float(input())
+b = float(input())
 
-list1 = [1, 2, 3]
-random.shuffle(list1)
-print("Shuffled:", list1)
+area = 0.5*(a+b)*h
+print(area)
+
+#3
+import math
+ns = int(input())
+lofs = float(input())
+area = (ns * lofs**2)/(4*math.tan(math.pi/ns))
+print(round(area))
+
+#4
+a = float(input())
+h = float(input())
+area = a*h
+print(area)
